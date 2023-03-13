7d1df91823c841335c722f23ac79131821fc8098
gruluk <g.luka98@gmail.com>
2023-03-13 19:25:13 +0100

    more firebase config

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { initializeApp } from 'firebase';
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseApp = initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyCQVfHMuBK_HHfYEwuYev1JrmAfhjWE9aM",
   authDomain: "lynetteportfolio-85320.firebaseapp.com",
   projectId: "lynetteportfolio-85320",
@@ -13,7 +13,7 @@ const firebaseApp = initializeApp({
   messagingSenderId: "922686727349",
   appId: "1:922686727349:web:6aac7ca51c7e9d3fd34afc",
   measurementId: "G-LLHMHVB12P"
-});
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
